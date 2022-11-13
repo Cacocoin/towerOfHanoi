e54abfb2821a9952516a4b47cf9d452bf6a09440
Cacocoin <98527172+Cacocoin@users.noreply.github.com>
2022-11-13 11:15:16 +0100

    Update towerOfHanoiArrow.js

--- towerOfHanoiArrow.js
+++ towerOfHanoiArrow.js
@@ -1,3 +1,8 @@
 //Tower of Hanoi algorithm to find the minimum moves for completion based on number of disks in puzzle, reduced and as an arrow function.
 
 towerHanoiArrow = (a) => a >= 0 ? ((2**(a-1)) + (2**(a-1))-1): 'enter a value greater than 0';
+
+
+//simpler still
+
+towerHanoiArrow = (a) => 2**a - 1
